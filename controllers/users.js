9be6375d4062afd6f5929d643c844e5dae91b171
vedanti <vedanti.r.bagade@gmail.com>
2025-01-24 19:09:54 +0530

    file added

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,7 @@ module.exports.signup = async (req, res) => {
       if (err) {
         return next(err);
       }
-      req.flash("success", "Welcome to TripTales!");
+      req.flash("success", "Welcome to WanderLust!");
       res.redirect("/listings");
     });
   } catch (e) {
@@ -27,7 +27,7 @@ module.exports.renderLoginForm = (req, res) => {
 };
 
 module.exports.login = async (req, res) => {
-  req.flash("success", "Welcome to TripTales!");
+  req.flash("success", "Welcome to WanderLust!");
   let redirectUrl = res.locals.redirectUrl || "/listings";
   res.redirect(redirectUrl);
 };
